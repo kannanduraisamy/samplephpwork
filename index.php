a47cb82618fad19addcaa10f6814e80fc2a6741d
kannanduraisamy <49671108+kannanduraisamy@users.noreply.github.com>
2019-04-16 13:28:37 +0530

    add approval

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>ec2 instance using beanstack for devops project- upated one</h1>
+<h1>ec2 instance using beanstack for devops project- upated one-adding approval stage</h1>
 
 <?php
 echo "version :Ver 1.0";
